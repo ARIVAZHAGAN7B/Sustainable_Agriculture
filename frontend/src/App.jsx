8e2f9567bc282aa7a4bae430565975c95571d021
ARIVAZHAGAN B <barivazhagan7@gmail.com>
2025-04-05 15:56:55 +0530

    Updated the api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ const FarmPredictor = () => {
 
   const handlePredict = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://farmiq-oj99.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
